e49cb580d9170c6889d586fffd307a3eb63511d0
Aaron Mensah <103023593+Aaronmensah33@users.noreply.github.com>
2026-02-10 09:29:10 +0100

    refactor: restructure App component and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* App.tsx is:
+
+Je layout (header, achtergrond, margins, etc.)
+
+Je navigatie/menu
+
+En de plek waar je bepaalt welke pagina wordt getoond met routes
+
+Alles wat de gebruiker ziet, loopt via dit bestand. 
+
+dit gaat naar main.tsx, dat rendert deze App component in het root element van index.html.
+*/
+
+
+import { Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* NavLink geeft automatisch een object met { isActive }
+  Als de link de huidige pagina is → isActive = true
+  Deze functie:
+  Bouwt een string met Tailwind classes
+
+  Als actief:
+  Witte achtergrond
+  Zwarte tekst
+
+  Anders:
+  Lichtere witte tekst
+  Hover effect
+  */
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // De hele pagina
+    <div className="min-h-screen bg-white text-black">
+      <Header />
+
+      <main className="mx-auto max-w-6xl px-6 py-8">
+
+          {/* Routes bepalen welke pagina getoond wordt 
+          als je naar / gaat toon Home component
+          als je naar /products gaat toon Products component
+          etc.
+          */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          {/* :id = dynamisch stuk, kan van alles zijn, in Product pagina kunnen we dat id gebruiken om te weten welk product we moeten tonen */}
+          <Route path="/ProductDetail/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+
+        {/*Alleen dit <main> stuk wisselt */}
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
+//Zodat main.tsx dit component kan renderen.
