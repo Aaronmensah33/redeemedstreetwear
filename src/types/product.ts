4db38871fbad64ffe5e3f27b6429daa27170c8ff
Aaron Mensah <103023593+Aaronmensah33@users.noreply.github.com>
2026-02-12 19:08:53 +0100

    feat: add User, CartItem types and enhance Product type with variants and categories

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,3 +1,15 @@
+export type ProductCategory = "jacket" | "hoodie" | "shirt" | "pants" | "vest";
+
+export type Size = "XS" | "S" | "M" | "L" | "XL";
+export type Color = "Black" | "Blue" | "Grey" | "White";
+
+export type ProductVariant = {
+  id: string;      // uniek per variant (handig voor backend)
+  size: Size;
+  color: Color;
+  stock: number;
+};
+
 export type Product = {
   id: string;
   name: string;
@@ -5,5 +17,8 @@ export type Product = {
   imageFront: string;
   imageBack: string;
   description: string;
+  category: ProductCategory;
+
+  variants: ProductVariant[];
 };
 
