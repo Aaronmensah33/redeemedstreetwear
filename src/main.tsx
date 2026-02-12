a2c07af0141aa162d48670aa3de619c65e2e4263
Aaron Mensah <103023593+Aaronmensah33@users.noreply.github.com>
2026-02-12 23:23:44 +0100

    hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,8 @@ dit gaat naar index.html, pakt het element met id "root" en rendert daar de hele
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App.tsx'
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
@@ -21,13 +22,13 @@ createRoot(document.getElementById('root')!).render(
   // StrictMode is een hulpprogramma dat helpt bij het opsporen van problemen in de applicatie. Het activeert extra controles en waarschuwingen voor zijn kinderen.
   <StrictMode>
     {/* BrowserRouter is een component dat de routing-functionaliteit van React Router inschakelt. Het maakt het mogelijk om verschillende pagina's te hebben binnen de applicatie zonder dat de pagina volledig opnieuw hoeft te laden. */}
-    <BrowserRouter>
+    <HashRouter>
         {/* App is het hoofdcomponent van de applicatie dat alle andere componenten bevat. Hier worden de routes en navigatie gedefinieerd. */}
       <AuthProvider>
       <CartProvider>
         <App />
       </CartProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
